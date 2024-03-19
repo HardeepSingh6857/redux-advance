7a4c6d3bcfc69edae8775474ea1fcfc675b48742
Hardeep Singh <iharry6857@gmail.com>
2024-03-19 13:20:04 +0530

    more coded way

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,20 +1,56 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { cartAction } from '../../store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
+  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const { id, title, price, description } = props;
 
   const AddToCartHandler = () => {
-    dispatch(cartAction.addItemToCart({
-      id,
-      title,
-      price,
-    }));
+    const newTotalQuantity = cart.totalQuantity + 1;
+    //cart.totalQuantity = cart.totalQuantity + 1; never do this outside of a reducer
+
+    const updatedItems = cart.items.slice();  //instead create a copy via slice to avoid mutation of data
+    const existingItem = updatedItems.find((item) => item.id === id);
+    if (existingItem) {
+      const updatedItem = {...existingItem};  //new object + copy existing properties
+      updatedItem.quantity++;
+      updatedItem.price = updatedItem.price + price;
+      const existingItemIndex = updatedItem.findIndex(
+        (item) => item.id === id
+      );
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({
+        id: id,
+        price: price,
+        quantity: 1,
+        totalPrice: price,
+        name: title,
+      });
+    }
+
+    const newCart = {
+      totalQuantity: newTotalQuantity,
+      items: updatedItems,
+    }
+
+    dispatch(cartAction.replaceCart(newCart));
+
+    //and then send http request
+    //  fetch('firebase-url', { method: 'POST',body: JSON.stringify(newCart) })
+
+    //  dispatch(  
+    //       cartActions.addItemToCart({
+    //         id,
+    //         title,
+    //         price,
+    //    })
+    //  };
   }
 
   return (
